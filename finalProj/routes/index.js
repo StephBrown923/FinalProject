146d3594991483dc1227768a65f09196407c92e7
StephBrown923 <stephy.mlbrown@gmail.com>
2016-03-21 23:52:19 +0000

    update

--- finalProj/routes/index.js
+++ finalProj/routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var mongodb = require('mongodb');
 
+var MongoClient = mongodb.MongoClient;
+
+var dbUrl = 'mongodb://localhost:27017/pokemon';
+
+var collection
+
+MongoClient.connect(dbUrl, function (err, db) {
+	if (err) {
+	  console.log('Unable to connect to the mongoDB server. Error:', err);
+	} else {
+	  console.log('Connection established to', dbUrl);
+
+	  collection = db.collection('tracker');
+
+	  collection.insert(tracker, function (err, result) {
+	    if (err) {
+		console.log(err);
+	    } else {
+		console.log('Inserted documents into the "tracker" collection. The documents inserted with "_id" are:', result.length, result);
+	   }
+	});
+	}
+});
+	
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/tracker', function(req, res) {
+	console.log("In Tracker");
+	collection.find().toArray(function(err, result) {
+		if(err) {
+		  console.log(err);
+		} else if(result.length) {
+		  console.log("Query worked");
+		  console.log(result);
+		  res.send(result);
+		} else {
+		  console.log("No Documents found");
+		}
+	});
+});
+
+router.get('/tracker', function(req, res) {
+	console.log("In Tracker Get");
+	res.send(tracker);
+	});
+
+router.post('/tracker', function(req, res) {
+	console.log("In Tracker Post");
+	console.log(req.body);
+	collection.insert(req.body, function(err, result) {
+		if (err) {
+		  console.log(err);
+		} else {
+		  console.log('Inserted documents into the "tracker" collection. The documents inserted with "_id" are:', result.length, result);
+		  res.end('{"csuccess" : "Updated Successfully", "status" : 200}');
+		}
+	});
 });
 
 module.exports = router;
+
+var tracker = [
+	{
+	  name: 'Ersebet Szabo',
+	  Birthplace: 'Borsod, Szendro Hungary'
+	}
+];
+
